26c09706facaf41a9fdf39f7d95f19b9a620f4c4
Aritz Ontalvilla <aritzontal@gmail.com>
2026-05-01 09:58:28 +0200

    proteccion rutas por rol + paágina admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import RegisterPage from './pages/RegisterPage'
+import AdminPage from './pages/AdminPage'
 import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
@@ -16,6 +17,15 @@ function App() {
         element={
           <ProtectedRoute>
             <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute allowedRoles={['ADMIN']}>
+            <AdminPage />
           </ProtectedRoute>
         }
       />
--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,15 +1,18 @@
 import { Navigate } from 'react-router-dom'
 import type { ReactNode } from 'react'
 import { useAuth } from '../contexts/AuthContext'
+import type { Role } from '../types/auth'
+
 
 interface ProtectedRouteProps {
     children: ReactNode
+    allowedRoles?: Role[]
 }
 
-function ProtectedRoute({ children }: ProtectedRouteProps) {
+function ProtectedRoute({ children, allowedRoles }: ProtectedRouteProps) {
     const { state } = useAuth()
 
-    // 1) Aún cargando la sesión desde localStorage → mostramos placeholder
+    // 1) Antes de nada cuando este cargando, mostramos placeholder
     if (state.isLoading) {
         return (
             <main className="min-h-screen flex items-center justify-center">
@@ -22,6 +25,12 @@ function ProtectedRoute({ children }: ProtectedRouteProps) {
     if (!state.user) {
         return <Navigate to="/login" replace />
     }
+
+    //En caso de que ROLE sea undefined, redirigimos a dashboard (así siempre tendran acceso lo logueados)
+    if (allowedRoles && !allowedRoles.includes(state.user.role)) {
+        return <Navigate to="/dashboard" replace />
+    }
+
 
     // 3) Cargado y hay sesión → deja pasar
     return <>{children}</>
